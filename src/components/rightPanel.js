28a085f8e4c94e596192f438a1485bb4c2c793c0
Ilja <ilja.solomatin@mail.ru>
2023-11-09 18:06:36 +0000

    Created left and right panels
    Created left and right panels containing icons with links to sites such as github and linkedin. Also added animations for fading in when the page loads.

---
+++ src/components/rightPanel.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./rightPanel.css";
+
+function RightPanel() {
+    return (
+        <div class="rightPanel">
+            <div class="rightPanelLink fade">
+                <a href="mailto:ilja.solomatin@bath.edu">ilja.solomatin@bath.edu</a>
+            </div>
+        </div>
+    );
+}
+
+export default RightPanel;
